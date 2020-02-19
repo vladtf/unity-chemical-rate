4fc327a091b462036ed405664de0adc7235b91d0
vladtf <vendeta0263@gmail.com>
2020-02-20 00:41:55 +0200

    Added credentials canvas

---
+++ Assets/GoToGithub.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToGithub : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void OnClick()
+    {
+        Application.OpenURL("https://github.com/vladtf/UnityChemicalSpeed");
+    }
+}
