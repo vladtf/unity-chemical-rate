9b654fd19dadac83ca780975ccd186f32a7b4aa7
vladtf <vendeta0263@gmail.com>
2020-03-01 22:02:19 +0200

    Added full presentation and deleted unnecessary files

---
+++ Assets/Scripts/PresentationScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PresentationScript : MonoBehaviour
+{
+    [SerializeField] private GameObject slide;
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private List<Texture2D> pictures;
+
+    private int imageIndex;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            imageIndex = 0;
+            slide.GetComponent<RawImage>().texture = pictures[imageIndex];
+
+            mainMenu.SetActive(true);
+            gameObject.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && imageIndex > 1)
+        {
+            imageIndex--;
+            slide.GetComponent<RawImage>().texture = pictures[imageIndex];
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) && imageIndex < pictures.Count-2)
+        {
+            imageIndex++;
+            slide.GetComponent<RawImage>().texture = pictures[imageIndex];
+        }
+
+    }
+}
