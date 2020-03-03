b30db28f68aff2c7602cf6dc57815d5db88d8be9
vladtf <vendeta0263@gmail.com>
2020-03-04 01:22:26 +0200

    Added chart asset to show the collisions on time

---
+++ Assets/Counter.cs
@@ -0,0 +1,48 @@
+﻿using AwesomeCharts;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+    [SerializeField] private Text collisionCounter;
+    [SerializeField] private float waitTime = 0.1f;
+    [SerializeField] private LineChart chart;
+    [SerializeField] private int MaxPointsOnChart = 20;
+
+    private float timer = 0.0f;
+    private int currentPoint = 0;
+
+    private int collisions;
+
+    private void Start()
+    {
+        chart.GetChartData().DataSets[0].AddEntry(new LineEntry(0, 0));
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > waitTime)
+        {
+            collisions = Int32.Parse(collisionCounter.text);
+            currentPoint++;
+
+            chart.GetChartData().DataSets[0].AddEntry(new LineEntry(currentPoint, collisions));
+
+            if (chart.GetChartData().DataSets[0].GetEntriesCount() > MaxPointsOnChart)
+            {
+                chart.GetChartData().DataSets[0].RemoveEntry(0);
+            }
+
+            chart.SetDirty();
+
+            collisionCounter.text = "0";
+
+            timer -= waitTime;
+        }
+    }
+}
